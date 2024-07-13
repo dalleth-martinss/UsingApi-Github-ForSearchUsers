912ba2f6e79f9b9febea803a9af995ba5ae4a5af
dalleth-martinss <essi_dalleth@outlook.com>
2024-07-13 15:31:48 -0300

    adding new class githubUser - endpoint

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,3 +1,18 @@
+export class GithubUser{
+   static search(username){
+      const endpoint = `https://api.github.com/users/${username}`
+      return fetch(endpoint)
+      .then(data => data.json())
+      .then(({login, name, public_repos, followers}) =>  // desestruturação objt data
+         ({
+         login,
+         name,
+         public_repos,
+         followers
+      }))
+   }
+}
+
 export class Favorites {
    constructor(root) {
       this.root = document.querySelector(root)
